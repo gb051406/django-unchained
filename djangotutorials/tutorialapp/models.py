a9f0cd3d89294786e97e51ed24268e3e381604c7
gb051406 <gb051406@wgcloud.org>
2024-04-15 10:36:29 -0500

    added teacher base

--- djangotutorials/tutorialapp/models.py
+++ djangotutorials/tutorialapp/models.py
@@ -16,4 +16,23 @@ class Student(models.Model):
     grade = models.CharField(max_length=200, null=True, choices=GRADES)
 
     def __str__(self):
-        return "" + str(self.lastname) + ", " + str(self.firstname) + ", Grade: " + str(self.grade)
+        return str(self.lastname) + ", " + str(self.firstname) + ", Grade: " + str(self.grade)
+class Teacher(models.Model):
+
+    SUBJECT =(
+        ('Fine Art', 'Fine Art'),
+        ('Math', 'Math'),
+        ('PE/Health', 'PE/Health'),
+        ('Practical Art', 'Practical Art'),
+        ('English', 'English'),
+        ('Social Studies', 'Social Studies'),
+        ('Science', 'Science'),
+    )
+
+    firstname = models.CharField(max_length=200, null=True)
+    lastname = models.CharField(max_length=200, null=True)
+    roomnumber = models.CharField(max_length=200, null=True)
+    subject = models.CharField(max_length=200, null=True, choices=SUBJECT)
+
+    def __str__(self):
+        return str(self.lastname) + ", " + str(self.firstname) + ", Subject: " + str(self.subject) + ", Room Number: " + str(self.roomnumber)
